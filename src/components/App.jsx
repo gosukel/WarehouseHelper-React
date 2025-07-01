54fe969257a19a57853db5f2735e659c37fb8b50
RJ Routh <94662443+gosukel@users.noreply.github.com>
2025-07-01 19:39:38 -0400

    add component directories

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+import mainPage from "./components/densityCalculatorComponents/mainPage/MainPage";
+
+function App() {
+    const [calcPage, setCalcPage] = useState(true);
+    return <></>;
+}
+
+export default App;
---
+++ src/components/densityCalculatorComponents/mainPage/MainPage.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+
+export default function MainPage() {
+    return <h1>Density Calculator</h1>;
+}
